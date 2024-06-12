efff945f36ed420479f730c755d27541d5456693
Nolan Jensen <130103322+Nolan-UCSC@users.noreply.github.com>
2024-06-12 16:10:50 -0700

    Updated title screen

--- Scenes/TitleScreen.js
+++ Scenes/TitleScreen.js
@@ -6,6 +6,7 @@ class TitleScreen extends Phaser.Scene {
 
     preload() {
         // Load any assets for the title screen if necessary
+        this.load.image('owl', './assets/owl.png'); // Make sure to provide the correct path to your image
     }
 
     create() {
@@ -25,10 +26,17 @@ class TitleScreen extends Phaser.Scene {
             fontSize: '24px',
             fill: '#ffffff'
         }).setOrigin(0.5);
-        let namessText = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY + 160, 'Nolan Jensen & Isaac Kim', {
+
+        let namesText = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY + 160, 'Nolan Jensen & Isaac Kim', {
             fontSize: '24px',
             fill: '#ffffff'
-        }).setOrigin(0.5);
+        }).setOrigin(0.5, 0.5);
+
+        // Add the owl images to the left and right of the authors' names
+        let owlImageLeft = this.add.image(this.cameras.main.centerX - namesText.width / 2 - 30, this.cameras.main.centerY + 160, 'owl');
+        let owlImageRight = this.add.image(this.cameras.main.centerX + namesText.width / 2 + 30, this.cameras.main.centerY + 160, 'owl');
+        owlImageLeft.setScale(0.5); // Adjust the scale as necessary
+        owlImageRight.setScale(0.5); // Adjust the scale as necessary
 
         // Add event listener for space key to start the game
         this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);

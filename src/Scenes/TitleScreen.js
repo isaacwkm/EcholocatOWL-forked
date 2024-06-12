2fbb3a79bfd8ae882ef25f265f66f08aa16b8a5f
Nolan Jensen <130103322+Nolan-UCSC@users.noreply.github.com>
2024-06-12 16:24:53 -0700

    updating the titleScreen

--- src/Scenes/TitleScreen.js
+++ src/Scenes/TitleScreen.js
@@ -5,7 +5,7 @@ class TitleScreen extends Phaser.Scene {
     }
 
     preload() {
-        // Load any assets for the title screen if necessary
+        this.load.image('owl', './assets/owl.png');
     }
 
     create() {
@@ -20,6 +20,22 @@ class TitleScreen extends Phaser.Scene {
             fontSize: '32px',
             fill: '#ffffff'
         }).setOrigin(0.5);
+
+        let authorsText = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY + 130, 'Authors:', {
+            fontSize: '24px',
+            fill: '#ffffff'
+        }).setOrigin(0.5);
+
+        let namesText = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY + 160, 'Nolan Jensen & Isaac Kim', {
+            fontSize: '24px',
+            fill: '#ffffff'
+        }).setOrigin(0.5, 0.5);
+
+        // Add the owl images note for Isaac change one to the wolf maybe
+        let owlImageLeft = this.add.image(this.cameras.main.centerX - namesText.width / 2 - 30, this.cameras.main.centerY + 160, 'owl');
+        let owlImageRight = this.add.image(this.cameras.main.centerX + namesText.width / 2 + 30, this.cameras.main.centerY + 160, 'owl');
+        owlImageLeft.setScale(0.5);
+        owlImageRight.setScale(0.5);
 
         // Add event listener for space key to start the game
         this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
